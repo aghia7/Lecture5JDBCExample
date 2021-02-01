02c9bace1f0b0fb16d492554f92d3c33550241ce
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2021-02-01 09:54:20 +0600

    First commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,41 @@ import com.company.repositories.interfaces.IUserRepository;
 public class Main {
 
     public static void main(String[] args) {
+//        String connectionUrl = "jdbc:postgresql://localhost:5432/simpledb";
+//        Connection con = null;
+//        ResultSet rs = null;
+//        Statement stmt = null;
+//        try {
+//            // Here we load the driver’s class file into memory at the runtime
+//            Class.forName("org.postgresql.Driver");
+//
+//            // Establish the connection
+//            con = DriverManager.getConnection(connectionUrl, "postgres", "0000");
+//
+//            // The object of statement is responsible to execute queries with the database
+//            stmt = con.createStatement();
+//
+//            // The executeQuery() method of Statement interface is used to execute queries
+//            // to the database. This method returns the object of ResultSet that can be
+//            // used to get all the records of a table that matches the sql statement
+//            rs = stmt.executeQuery("select * from users");
+//
+//            while (rs.next()) // Processing the result
+//                System.out.println(rs.getInt("id") + "  "
+//                        + rs.getString("name") + "  " + rs.getString("surname"));
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        } finally {
+//
+//            try { // Close connection - clean up the system resources
+//                rs.close();
+//                stmt.close();
+//                con.close();
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//            }
+//        }
+
         IDB db = new DB();
         IUserRepository repo = new UserRepository(db);
         MyApplication app = new MyApplication(repo);
